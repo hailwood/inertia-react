2673e4416bd5bf8cbafd01aea4dc963205251d46
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2019-04-23 17:14:25 +0200

    Add usePageProps hook

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Inertia from 'inertia'
 import App from './App'
 import InertiaLink from './InertiaLink'
 import PageContext from './PageContext'
+import usePageProps from './usePageProps'
 
 export default App
-export { Inertia, InertiaLink, PageContext }
+export { Inertia, InertiaLink, PageContext, usePageProps }
---
+++ src/usePageProps.js
@@ -0,0 +1,8 @@
+import { useContext } from 'react';
+import PageContext from './PageContext';
+
+export default function usePageProps() {
+  const { props } = useContext(PageContext);
+
+  return props;
+}
