5e00a7a84a5d310654556c939e9907c20888ccc7
Michaël De Boey <info@michaeldeboey.be>
2019-05-14 19:35:20 +0200

    No comma in interfaces

--- index.d.ts
+++ index.d.ts
@@ -2,25 +2,25 @@ import Inertia, { Page as InertiaPage } from 'inertia'
 import { Dispatch, FC, Key, ReactNode, SetStateAction } from 'react'
 
 interface AppProps<PageProps = {}> {
-  children?: ({ Component: ReactNode, key: Key, props: PageProps }) => ReactNode,
-  initialPage: InertiaPage,
-  resolveComponent: (name: string) => ReactNode,
+  children?: ({ Component: ReactNode, key: Key, props: PageProps }) => ReactNode
+  initialPage: InertiaPage
+  resolveComponent: (name: string) => ReactNode
 }
 interface App<PageProps = {}> extends FC<AppProps<PageProps>> {}
 
 interface InertiaLinkProps {
-  children?: ReactNode,
-  href: string,
-  method?: string,
-  onClick?: () => void,
-  preserveScroll?: boolean,
-  replace?: boolean,
+  children?: ReactNode
+  href: string
+  method?: string
+  onClick?: () => void
+  preserveScroll?: boolean
+  replace?: boolean
 }
 declare const InertiaLink: FC<InertiaLinkProps>
 
 interface Page<PageProps = {}> {
-  instance: ReactNode | null,
-  props: PageProps | {},
+  instance: ReactNode | null
+  props: PageProps | {}
 }
 
 declare function usePage<PageProps = {}>(): Page<PageProps>
